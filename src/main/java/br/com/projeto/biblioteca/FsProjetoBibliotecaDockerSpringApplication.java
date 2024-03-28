c8a23b36efce26aacf5b637ad5ed450aedc9282c
Vinicius Goulart Alves <vini.goulartalves@gmail.com>
2024-03-28 13:50:06 -0300

    Initial commit

---
+++ src/main/java/br/com/projeto/biblioteca/FsProjetoBibliotecaDockerSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsProjetoBibliotecaDockerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsProjetoBibliotecaDockerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/projeto/biblioteca/FsProjetoBibliotecaDockerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsProjetoBibliotecaDockerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
