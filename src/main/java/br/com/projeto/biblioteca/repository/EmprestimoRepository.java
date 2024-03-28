e8bc67160a1f15bc74ec8dd478ad5f21221d64a4
Vinicius Goulart Alves <vini.goulartalves@gmail.com>
2024-03-28 19:04:56 -0300

    feat: QUERY - UPDATE

--- src/main/java/br/com/projeto/biblioteca/repository/EmprestimoRepository.java
+++ src/main/java/br/com/projeto/biblioteca/repository/EmprestimoRepository.java
@@ -1,11 +1,31 @@
 package br.com.projeto.biblioteca.repository;
 
 import br.com.projeto.biblioteca.entities.EmprestimoEntity;
+import br.com.projeto.biblioteca.entities.LivroEntity;
+import br.com.projeto.biblioteca.entities.MembroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface EmprestimoRepository extends JpaRepository<EmprestimoEntity, Long> {
 
+    @Modifying
+    @Query("UPDATE EmprestimoEntity emprestimo " +
+            "SET " +
+            "emprestimo.livro = :livro, " +
+            "emprestimo.membro = :membro, " +
+            "emprestimo.dataEmprestimo = :dataEmprestimo, " +
+            "emprestimo.dataDevolucao = :dataDevolucao " +
+            "WHERE emprestimo.id = :id")
+    void update(@Param("id") Long id,
+                @Param("livro") LivroEntity livro,
+                @Param("membro") MembroEntity membro,
+                @Param("dataEmprestimo") LocalDate dataEmprestimo,
+                @Param("dataDevolucao") LocalDate dataDevolucao);
 
 }
--- src/main/java/br/com/projeto/biblioteca/repository/VisitanteRepository.java
+++ src/main/java/br/com/projeto/biblioteca/repository/VisitanteRepository.java
@@ -1,10 +1,25 @@
 package br.com.projeto.biblioteca.repository;
 
+
 import br.com.projeto.biblioteca.entities.VisitanteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VisitanteRepository extends JpaRepository<VisitanteEntity, Long> {
+
+
+    @Modifying
+    @Query("UPDATE VisitanteEntity visitante " +
+            "SET " +
+            "visitante.nome = :nome, " +
+            "visitante.telefone = :telefone " +
+            "WHERE visitante.id = :id")
+    void update(@Param("id") Long id,
+                @Param("nome") String nome,
+                @Param("telefone") String telefone);
 
 }
